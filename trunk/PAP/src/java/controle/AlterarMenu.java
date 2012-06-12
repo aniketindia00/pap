4ad6fe03c3fdf8aa488329f01ab8ac06d0065635
andrebsguedes@gmail.com <andrebsguedes@gmail.com@04aa5a43-ecb5-5a77-ecf8-c48016c198cb>
2012-06-12 22:55:50 +0000

    Alteração André

--- trunk/PAP/src/java/controle/AlterarMenu.java
+++ trunk/PAP/src/java/controle/AlterarMenu.java
@@ -34,7 +34,7 @@ public class AlterarMenu extends HttpServlet {
         PrintWriter out = response.getWriter();
         HttpSession session = request.getSession();
         try {
-    if(!(Boolean) session.getAttribute("menu")){
+    if(session.getAttribute("menu") == null){
        response.sendRedirect("index.jsp?erro=1");
     }else{
             // TODO output your page here
