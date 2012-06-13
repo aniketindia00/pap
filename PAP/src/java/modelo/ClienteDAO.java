46913d0bc1c3745b2ba378d445a9f76d024019d0
andrebsguedes <andrebsguedes@gmail.com>
2012-06-13 19:13:28 +0000

    Alteração André

--- PAP/src/java/modelo/ClienteDAO.java
+++ PAP/src/java/modelo/ClienteDAO.java
@@ -1,8 +1,20 @@
 package modelo;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class ClienteDAO extends DataBaseDAO {
 
     public ClienteDAO() throws Exception {
     }
 
-    
+        public void inserir(Cliente c) throws SQLException{
+
+        PreparedStatement pst;
+        String sql ="INSERT INTO cliente(perfil) VALUES(?)";
+        pst =conn.prepareStatement(sql);
+        pst.setString(1,p.getPerfil());
+        pst.execute();
+
+    }
 }
