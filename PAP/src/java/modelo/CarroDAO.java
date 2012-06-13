a03e86b81ec9e7767ac74b8eeafa84a6934960c5
andrebsguedes <andrebsguedes@gmail.com>
2012-06-13 18:27:15 +0000

    Alteração André

---
+++ PAP/src/java/modelo/CarroDAO.java
@@ -0,0 +1,22 @@
+package modelo;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class CarroDAO extends DataBaseDAO {
+
+    public CarroDAO() throws Exception {
+    }
+
+    public void inserir(Carro c) throws SQLException{
+        PreparedStatement pst;
+        String sql = "INSERT INTO usuario (id_cliente,modelo,ano,marca) values(?,?,?,?)";
+        pst = conn.prepareStatement(sql);
+        pst.setInt(1,c.getIdCliente());
+        pst.setString(2, c.getModelo());
+        pst.setString(3, c.getAno());
+        pst.setString(4, c.getMarca());
+        pst.execute();
+    }
+
+}
