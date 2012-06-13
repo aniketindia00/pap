58d9edc140c670a3fa6d64ca5134cad1a838fb15
andrebsguedes <andrebsguedes@gmail.com>
2012-06-13 17:47:41 +0000

    Alteração André

--- PAP/src/java/modelo/Cliente.java
+++ PAP/src/java/modelo/Cliente.java
@@ -1,16 +1,22 @@
 package modelo;
 
+import java.util.ArrayList;
+
 public class Cliente extends Pessoa{
 
     private int id;
-
+    private ArrayList<Carro> carros;
+    
     public Cliente() {
     }
 
-    public Cliente(int id, String nome, String telefone) {
-        super(nome,telefone);
+    public Cliente(int id, ArrayList<Carro> carros, String nome, String telefone) {
+        super(nome, telefone);
         this.id = id;
+        this.carros = carros;
     }
+
+
 
 
     public int getId() {
@@ -19,6 +25,14 @@ public class Cliente extends Pessoa{
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public ArrayList<Carro> getCarros() {
+        return carros;
+    }
+
+    public void setCarros(ArrayList<Carro> carros) {
+        this.carros = carros;
     }
 
     
