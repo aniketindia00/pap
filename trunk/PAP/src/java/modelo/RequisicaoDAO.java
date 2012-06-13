246df2e1d68862bfe18e5978a6da726c980538f8
dandandandan1102@gmail.com <dandandandan1102@gmail.com@04aa5a43-ecb5-5a77-ecf8-c48016c198cb>
2012-06-13 20:03:09 +0000

    alteração feita por Daniel

--- trunk/PAP/src/java/modelo/RequisicaoDAO.java
+++ trunk/PAP/src/java/modelo/RequisicaoDAO.java
@@ -26,7 +26,7 @@ public class RequisicaoDAO extends DataBaseDAO{
         pst.setInt(1,r.getId());
         pst.setString(2, r.getCpfMecanico());
         pst.setString(3, r.getDataEmissao());
-        pst.setString(4, r.getDataEmissao());
+        pst.setString(4, r.getHoraEmissao());
         pst.setDouble(5, r.getValor());
         pst.execute();
 
