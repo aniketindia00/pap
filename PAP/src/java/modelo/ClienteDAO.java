856451abe6e5f0477fd40fb8f1f2a564a07cff8a
andrebsguedes <andrebsguedes@gmail.com>
2012-06-13 19:27:49 +0000

    Alteração André

--- PAP/src/java/modelo/ClienteDAO.java
+++ PAP/src/java/modelo/ClienteDAO.java
@@ -1,20 +1,44 @@
 package modelo;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class ClienteDAO extends DataBaseDAO {
 
     public ClienteDAO() throws Exception {
     }
 
-        public void inserir(Cliente c) throws SQLException{
-
+    public void inserir(Cliente c) throws SQLException{
         PreparedStatement pst;
-        String sql ="INSERT INTO cliente(perfil) VALUES(?)";
+        String sql ="INSERT INTO cliente(nome,telefone) VALUES(?,?)";
         pst =conn.prepareStatement(sql);
-        pst.setString(1,p.getPerfil());
+        pst.setString(1,c.getNome());
+        pst.setString(1,c.getTelefone());
         pst.execute();
 
     }
+    public ArrayList<Cliente> listar() throws SQLException, Exception{
+        CarroDAO cDB = new CarroDAO();
+        ArrayList<Cliente> lista = new ArrayList<Cliente>();
+        PreparedStatement pst;
+        String sql = "SELECT * FROM cliente";
+        pst = conn.prepareStatement(sql);
+        ResultSet rs = pst.executeQuery();
+        while(rs.next()){
+            Cliente c = new Cliente();
+            c.setId(rs.getInt("id"));
+            c.setNome(rs.getString("nome"));
+            c.setTelefone(rs.getString("telefone"));
+            cDB.conectar();
+            c.setCarros(cDB.carrosCliente(c.getId()));
+            cDB.desconectar();
+            lista.add(c);
+        }
+        return lista;
+
+    }
+
+
 }
